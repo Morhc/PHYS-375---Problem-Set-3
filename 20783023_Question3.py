810d6adaf9bbf2e258da6084f654c82cc02bca3b
joissa <itsjoshissa@gmail.com>
2022-02-20 18:30:54 -0500

    completed part h

--- 20783023_Question3.py
+++ 20783023_Question3.py
@@ -57,6 +57,8 @@ def part_e(savepath=""):
     else:
         plt.savefig(savepath)
 
+    plt.close('all')
+
 def part_g():
     """Assuming n=3, Rstar = 1Rsun, and Mstar = 1Mstar; we find alpha, rho_c, and K
     INPUTS:
@@ -86,24 +88,56 @@ def part_g():
     #we can use the alpha (and convert gamma - 2 = 1/n -1)
     K = (alpha**2)*4*np.pi*G/(np.power(rho_c, 1/n - 1)*(n+1))
 
-    return alpha, rho_c, K
+    return alpha, rho_c, K, x_0
 
-def part_h(alpha, rho_c, K, savepath=""):
+def part_h(alpha, rho_c, K, x_0, savepath=""):
     """Solves the Lane-Emden equation numerically for various n and plots it.
     INPUTS:
+        alpha - scaling ratio between x and r.
+        rho_c - the central density of the star.
+        K - a constant for the polytropic equation of state.
+        x_0 - the radius of the star in x.
         savepath - Path to save the plot to. If nothing is given, it will just display.
     OUTPUTS:
         Outputs a plot of the density, pressure, and temperature as a function of r/Rstar
     """
 
     X, Y = 0.55, 0.4
+    mu = np.power(2*X + 3*Y/4, -1) #from Lecture 9a; assuming Z = 0
+
     #grabbed from p. 576 of Ryden
     R = 6.955e8
+    #grabbed from p. 575 of Ryden
+    mp = 1.673e-27
+    k = 1.381e-23
+
+    n = 3
+
+    #note: r/Rstar = x/x_0, so we can plot over that scale :)
+    x_reduced = np.linspace(0, x_0, 1000)/x_0
+
+    thetas, dthetas = compute_theta(x_reduced, n)
+
+    rho = (np.power(thetas, n)*rho_c)
+
+    #since we assume a polytropic equation, we know P(r) = K*p(r)^(1/n + 1)
+    P = K * np.power(rho, 1/n + 1)
+
+    #we get temperature by rearranging Equation 15.79 from Ryden
+    T = P*mu*mp/(k*rho)
+
+    plt.plot(x_reduced, T, label=r'$T$($r$)')
+    plt.plot(x_reduced, P, label=r'$P$($r$)')
+    plt.plot(x_reduced, rho, label=r'$\rho$($r$)')
+
+    plt.legend(loc='best')
 
     if savepath == "":
         plt.show()
     else:
         plt.savefig(savepath)
+
+    plt.close('all')
 
 def main():
 
@@ -112,11 +146,11 @@ def main():
     e_path = os.path.join(here, 'PS3-Q3e.png')
     part_e(e_path)
 
-    alpha, rho_c, K = part_g()
+    alpha, rho_c, K, x_0 = part_g()
     print(f'The solved variables are:\n\talpha = {alpha}\n\trho_c = {rho_c}\n\tK = {K}\n')
 
     h_path = os.path.join(here, 'PS3-Q3h.png')
-    part_h(alpha, rho_c, K, h_path)
+    part_h(alpha, rho_c, K, x_0, h_path)
 
 if __name__ == '__main__':
     main()
